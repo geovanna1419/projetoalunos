10689490ebffb2c2c13049609f30982462277487
geovanna1419 <geovannatrabalhos19@gmail.com>
2024-09-18 11:58:59 -0300

    Add files via upload

---
+++ SQL Editor.sql
@@ -0,0 +1,34 @@
+﻿create database projetoalunos_db;
+use projetoalunos_db;
+
+
+CREATE TABLE tbl_professores
+   id_professores int NOT NULL AUTO_INCREMENT,
+   prof_nome VARCHAR (45),
+   prof_endereço VARCHAR (80)
+   prof_email VARCHAR (45)
+   prof_telefone DECIMAL (9)
+   prof_cpf DECIMAL (11)
+   prof_nascimento DATE 
+CREATE TABLE tbl_cidades
+   id_cidades int NOT NULL AUTO_INCREMENT,
+   cid_nome VARCHAR (45)
+   cid_uf VARCHAR  (2)
+CREATE TABLE tbl_alunos 
+   id_alunos int NOT NULL AUTO_INCREMENT,
+   prof_nome VARCHAR (45)
+   prof_endereço VARCHAR (80)
+   prof_email VARCHAR (45)
+   prof_telefone DECIMAL (9)
+   prof_cpf DECIMAL (11)
+   prof_nascimento DATE 
+CREATE TABLE tbl_cursos 
+   id_cursos int NOT NULL AUTO_INCREMENT,
+   cur_nome
+   cur_valor
+CREATE TABLE tbl_usuarios (
+  id_usuarios int NOT NULL AUTO_INCREMENT,
+  usu_nome varchar(45)
+  usu_username VARCHAR(45),
+  usu_senha varchar(45)
+);
---
+++ principal.py
@@ -0,0 +1,8 @@
+import tkinter
+from tkinter import *
+
+class principal :
+    def__init__(self,master = none ):
+        self.master = master
+        self.mster.title("gereciar alunos")
+    
